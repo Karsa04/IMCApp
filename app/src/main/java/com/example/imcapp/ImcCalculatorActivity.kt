418677a95af698c74a9f7fd7b22b32bf2bcb6c74
Karsa04 <carlessaezna04@gmai.com>
2024-10-29 13:42:50 +0100

    1.1(lógica cardv casi completa)

--- app/src/main/java/com/example/imcapp/ImcCalculatorActivity.kt
+++ app/src/main/java/com/example/imcapp/ImcCalculatorActivity.kt
@@ -5,46 +5,55 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class ImcCalculatorActivity : AppCompatActivity() {
 
     private lateinit var maleview: CardView
     private lateinit var femaleview: CardView
+    var  isMaleSelected=true
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        initComponents()
-        initListeners()
+
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_imc_calculator)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        initComponents()
+        initListeners()
+        initUI()
     }
     private fun initComponents() {
         maleview = findViewById(R.id.maleview)
         femaleview = findViewById(R.id.femaleview)
     }
 
-    private fun initListeners(){
-        var isMaleSelected=true
+    private fun initListeners() {
+
+        maleview.setOnClickListener{
         setGenderColor()
+    }
+        femaleview.setOnClickListener{
+            setGenderColor()
+        }
+
+
     }
     private fun setGenderColor(){
         maleview.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
         femaleview.setCardBackgroundColor(getBackgroundColor(!isMaleSelected))
     }
-    private fun getBackgroundColor(isMaleSelected:  ){
+    private fun getBackgroundColor(isMaleSelected: Boolean): Int {
         val colorReference = if(isMaleSelected) {
             R.color.bg_comp_sel
         } else {
             R.color.bg_comp
         }
-        return ContextCompat.getColor(this,R)
+        return ContextCompat.getColor(this,colorReference)
+    }
+
+    private fun initUI(){
+        setGenderColor()
     }
 }
