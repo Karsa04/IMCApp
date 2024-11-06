94a171c1838c01ead9b6cca58ab1f483600d6ef4
Karsa04 <carlessaezna04@gmai.com>
2024-11-06 12:46:05 +0100

    1.3

--- app/src/main/java/com/example/imcapp/ImcCalculatorActivity.kt
+++ app/src/main/java/com/example/imcapp/ImcCalculatorActivity.kt
@@ -1,16 +1,29 @@
 package com.example.imcapp
 
+import android.icu.text.DecimalFormat
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.slider.RangeSlider
 
 
 class ImcCalculatorActivity : AppCompatActivity() {
 
     private lateinit var maleview: CardView
     private lateinit var femaleview: CardView
+    private lateinit var tvHeight:TextView
+    private lateinit var rsHeight:RangeSlider
+    private lateinit var edad:TextView
+    private lateinit var peso:TextView
+    private lateinit var btnAddAge: FloatingActionButton
+    private lateinit var btnAddAge2: FloatingActionButton
+    private lateinit var btnSubtractAge: FloatingActionButton
+    private lateinit var btnSubtractAge2: FloatingActionButton
+
     var  isMaleSelected=true
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +40,8 @@ class ImcCalculatorActivity : AppCompatActivity() {
     private fun initComponents() {
         maleview = findViewById(R.id.maleview)
         femaleview = findViewById(R.id.femaleview)
+        rsHeight = findViewById(R.id.rsHeight)
+        tvHeight= findViewById(R.id.tvHeight)
     }
 
     private fun initListeners() {
@@ -36,6 +51,11 @@ class ImcCalculatorActivity : AppCompatActivity() {
     }
         femaleview.setOnClickListener{
             setGenderColor(false)
+        }
+        
+        rsHeight.addOnChangeListener {_,value,_ ->
+
+            tvHeight.text= DecimalFormat("#.##").format(value)+"cm"
         }
 
 
