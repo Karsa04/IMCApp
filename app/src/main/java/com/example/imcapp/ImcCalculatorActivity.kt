2238c0f6530137440b1b8e81521f09eee6f8e2f7
Karsa04 <carlessaezna04@gmai.com>
2024-11-07 12:00:16 +0100

    1.4(cardV peso y edad)

--- app/src/main/java/com/example/imcapp/ImcCalculatorActivity.kt
+++ app/src/main/java/com/example/imcapp/ImcCalculatorActivity.kt
@@ -17,14 +17,16 @@ class ImcCalculatorActivity : AppCompatActivity() {
     private lateinit var femaleview: CardView
     private lateinit var tvHeight:TextView
     private lateinit var rsHeight:RangeSlider
-    private lateinit var edad:TextView
-    private lateinit var peso:TextView
+    private lateinit var edadNum:TextView
+    private lateinit var pesoNum:TextView
+    private lateinit var btnAddWeight: FloatingActionButton
     private lateinit var btnAddAge: FloatingActionButton
-    private lateinit var btnAddAge2: FloatingActionButton
+    private lateinit var btnSubtractWeight: FloatingActionButton
     private lateinit var btnSubtractAge: FloatingActionButton
-    private lateinit var btnSubtractAge2: FloatingActionButton
 
-    var  isMaleSelected=true
+    var  operacion =true
+    private var Weight =0
+    private var age=0
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -42,6 +44,12 @@ class ImcCalculatorActivity : AppCompatActivity() {
         femaleview = findViewById(R.id.femaleview)
         rsHeight = findViewById(R.id.rsHeight)
         tvHeight= findViewById(R.id.tvHeight)
+        edadNum= findViewById(R.id.edadNum)
+        pesoNum= findViewById(R.id.pesoNum)
+        btnAddWeight= findViewById(R.id.btnAddAge)
+        btnSubtractWeight= findViewById(R.id.btnSubtractAge)
+        btnAddAge= findViewById(R.id.btnaddAge2)
+        btnSubtractAge= findViewById(R.id.btnSubtractAge2)
     }
 
     private fun initListeners() {
@@ -57,6 +65,25 @@ class ImcCalculatorActivity : AppCompatActivity() {
 
             tvHeight.text= DecimalFormat("#.##").format(value)+"cm"
         }
+        btnAddWeight.setOnClickListener {
+            operacion=true
+            setWeight(operacion)
+        }
+
+        btnAddAge.setOnClickListener {
+            operacion=true
+            setAge(operacion)
+        }
+
+        btnSubtractWeight.setOnClickListener {
+            operacion=false
+            setWeight(operacion)
+        }
+
+        btnSubtractAge.setOnClickListener {
+            operacion=false
+            setAge(operacion)
+        }
 
 
     }
@@ -64,6 +91,25 @@ class ImcCalculatorActivity : AppCompatActivity() {
         maleview.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
         femaleview.setCardBackgroundColor(getBackgroundColor(!isMaleSelected))
     }
+    private fun setWeight(operacion:Boolean){
+        if (operacion){
+            this.Weight+=1
+            pesoNum.setText(""+this.Weight)
+        }else{
+            this.Weight-=1
+            pesoNum.setText(""+this.Weight)
+        }
+
+    }  private fun setAge(operacion:Boolean){
+        if (operacion){
+            this.age+=1
+            edadNum.setText(""+this.age)
+        }else{
+            this.age-=1
+            edadNum.setText(""+this.age)
+        }
+    }
+
     private fun getBackgroundColor(isMaleSelected: Boolean): Int {
         val colorReference = if(isMaleSelected) {
             R.color.bg_comp_sel
@@ -75,5 +121,7 @@ class ImcCalculatorActivity : AppCompatActivity() {
 
     private fun initUI(){
         setGenderColor(true)
+        setAge(operacion)
+        setWeight(operacion)
     }
 }
